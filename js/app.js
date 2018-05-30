6c5096814880911820949f9a31b4abe7651a4d1a
Tamila <itstamila@gmail.com>
2018-05-29 23:44:01 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,162 @@
+/*
+ * Create a list that holds all of your cards
+ */
+var cards = ['fa-diamond', 'fa-diamond',
+             'fa-paper-plane-o', 'fa-paper-plane-o',
+             'fa-anchor', 'fa-anchor',
+             'fa-bolt', 'fa-bolt',
+             'fa-cube', 'fa-cube',
+             'fa-leaf', 'fa-leaf',
+             'fa-bicycle', 'fa-bicycle',
+             'fa-bomb', 'fa-bomb'
+            ];
+
+function generateCard(card) {
+  return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+}
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+/*flip cards */
+
+function initGame(){
+  var deck = document.querySelector('.deck');
+  var cardHTML = shuffle(cards).map(function(card){
+    return generateCard(card);
+  });
+
+
+  deck.innerHTML = cardHTML.join('')
+}
+initGame();
+
+var allCards = document.querySelectorAll('.card');
+var openCards =[];
+var moves = 0;
+
+allCards.forEach(function(card) {
+   card.addEventListener('click', function(e) {
+          if (!card.classList.contains('open')&& !card.classList.contains('show') && !card.classList.contains('match')) {
+            openCards.push(card);
+            card.classList.add('open', 'show');
+
+            //Do match
+
+            if (openCards.length == 2) {
+              moveCount();
+              //DO MATCH
+              if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                  openCards[0].classList.add('match');
+                  openCards[0].classList.add('open');
+                  openCards[0].classList.add('show');
+
+                  openCards[1].classList.add('match');
+                  openCards[1].classList.add('open');
+                  openCards[1].classList.add('show');
+
+                  openCards = [];
+              }else {
+                setTimeout(function(){
+                  openCards.forEach(function(card){
+                    card.classList.remove ('open','show');
+                  });
+
+                  openCards = [];
+                },1000);
+              }
+            }
+          }
+  });
+});
+
+//Count moves
+var moves = 0;
+var moveCounter = document.querySelector('.moves');
+var stars = document.querySelector('.stars');
+
+
+function moveCount() {
+  moves++;
+  moveCounter.innerHTML = moves;
+  if (moves == 1) {
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+  if (moves >= 14) {
+    stars.removeChild(stars.childNodes[1]);
+  }
+}
+
+var second = 0,
+    minute = 0;
+hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+
+function startTimer() {
+    interval = setInterval(function() {
+        timer.innerHTML = minute + " min " + second + " sec";
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
+}
+
+
+function congratulations() {
+    if (openCards.length == 16) {
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+
+        // show congratulations modal
+        modal.classList.add("show");
+
+        // declare star rating variable
+        var starRating = document.querySelector(".stars").innerHTML;
+
+        //showing move, rating, time on modal
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+        document.getElementById("totalTime").innerHTML = finalTime;
+
+    };
+}
